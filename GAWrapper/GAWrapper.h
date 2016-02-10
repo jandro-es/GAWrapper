d937eb07066f8b599c7696b9ad64dfd29110c8d6
Alejandro Barros Cuetos <jandrob1978@gmail.com>
2016-02-10 21:34:26 +0000

    Initial Commit

---
+++ GAWrapper/GAWrapper.h
@@ -0,0 +1,19 @@
+//
+//  GAWrapper.h
+//  GAWrapper
+//
+//  Created by Alejandro Barros Cuetos on 10/02/2016.
+//  Copyright © 2016 Alejandro Barros Cuetos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GAWrapper.
+FOUNDATION_EXPORT double GAWrapperVersionNumber;
+
+//! Project version string for GAWrapper.
+FOUNDATION_EXPORT const unsigned char GAWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GAWrapper/PublicHeader.h>
+
+
